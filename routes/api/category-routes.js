22a9b67d75aa55e792b6f9c8b758d43fa54f4f3b
laurenceokite <ljsokite@gmail.com>
2020-08-23 21:48:57 -0500

    inital commit

---
+++ routes/api/category-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll({
+    include: [{
+      model: Product,
+      attributes: ['product_name'] 
+    }]
+  }).then(categories => {
+    res.json(categories);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  }); 
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Product,
+      attributes: ['product_name'] 
+    }]
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  }); 
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value\
+  Category.findOne(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  }); 
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  }); 
+});
+
+module.exports = router;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,66 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [{
+      model: Product,
+      attributes: ['product_name'] 
+    }]
+  }).then(tag => {
+    res.json(tag);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Product,
+      attributes: ['product_name'] 
+    }]
+  }).then(tag => {
+    res.json(tag);
+  }); 
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create({
+    tag_name: req.body.category_name
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.findOne(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(tag => {
+    res.json(tag);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(tag => {
+    res.json(tag);
+  }); 
+});
+
+module.exports = router;
